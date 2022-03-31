bb4ca9b5995c5994ed27d9ddbf7f8f766bad138c
Aleksei Krasikov <89021443+krasetm12@users.noreply.github.com>
2022-03-31 10:38:33 +0300

    comment

--- src/test/java/TestEtm.java
+++ src/test/java/TestEtm.java
@@ -1,15 +1,15 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 public class TestEtm{
   @Epic("Testing FOR https://idev.etm.ru/ipro3/ tasks ")
@@ -23,14 +23,16 @@ public class TestEtm{
           "5-закрытие формы")
   @Test
   public void testGoogleSearch() throws InterruptedException {
-        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com/");
-        Thread.sleep(5000);	// Let the user actually see something!
-        WebElement searchBox = driver.findElement(By.name("q")); searchBox.sendKeys("ChromeDriver"); searchBox.submit();
-        Thread.sleep(5000);	// Let the user actually see something!
-        driver.quit();
+    // Optional. If not specified, WebDriver searches the PATH for chromedriver.
+    System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://www.google.com/");
+    Thread.sleep(5000);  // Let the user actually see something!
+    WebElement searchBox = driver.findElement(By.name("q"));
+    searchBox.sendKeys("ChromeDriver");
+    searchBox.submit();
+    Thread.sleep(5000);  // Let the user actually see something!
+    driver.quit();
 
 // String SITE_URL="https://idev.etm.ru/ipro3";
 //  WebDriver driver;
@@ -50,15 +52,15 @@ public class TestEtm{
 //  public void finish(){
 //    driver.quit();
 //  }
-  @Epic("Testing FOR https://idev.etm.ru/ipro3/ tasks ")
-  @Feature(value= "Tests for task-basket functionality for market-user")
-  @Severity(SeverityLevel.MINOR)
-  @Description("проверка формы добавить товар-" +
-          "1-вводим логин пароль-" +
-          "2-переходим в корзину-" +
-         "3-кликаем на форму Добавить товар-" +
-         "4-проверяем все поля формы на кликабельность"+
-         "5-закрытие формы")
+//  @Epic("Testing FOR https://idev.etm.ru/ipro3/ tasks ")
+//  @Feature(value= "Tests for task-basket functionality for market-user")
+//  @Severity(SeverityLevel.MINOR)
+//  @Description("проверка формы добавить товар-" +
+//          "1-вводим логин пароль-" +
+//          "2-переходим в корзину-" +
+//         "3-кликаем на форму Добавить товар-" +
+//         "4-проверяем все поля формы на кликабельность"+
+//         "5-закрытие формы")
 //  @Test
 //
 //
@@ -91,4 +93,5 @@ public class TestEtm{
 //
 //  }
   }
-        }
+}
+
